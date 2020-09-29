bb03fb627e31865c58bed28ab3424b0f19ceacfa
Brianne <brianne.ewell1@gmail.com>
2020-09-29 17:39:14 -0600

    added variables for password critera

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var CharNumber = prompt("How many characters should your password contain?")
+var confirmNumbers = confirm("Click ok to include numbers");
+  var confirmLower = confirm("Click ok to include lower case letters");
+  var confirmUpper = confirm ("Click ok to include uppercase letters");
+  var confirmSpecial = confirm ("Click ok to include special characters");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  if (confirmNumbers === true) {
+   for (var i = 0; i < 10; i++) 
+    var num = math.floor(math.random() * 10) + 1;
+  }
+
 
   passwordText.value = password;
 
@@ -12,3 +24,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
