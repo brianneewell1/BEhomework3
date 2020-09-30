933cf9fc86e19f369fb54158ee955ee4492bb8b6
Brianne <brianne.ewell1@gmail.com>
2020-09-29 20:57:00 -0600

    updated Javascript

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Global Variables
+let userChar = [];
+let userPass = [];
 
-var CharNumber = prompt("How many characters should your password contain?")
-var confirmNumbers = confirm("Click ok to include numbers");
-  var confirmLower = confirm("Click ok to include lower case letters");
-  var confirmUpper = confirm ("Click ok to include uppercase letters");
-  var confirmSpecial = confirm ("Click ok to include special characters");
+//System Variables
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let special = "!@#$%&*()-?_";
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  if (confirmNumbers === true) {
-   for (var i = 0; i < 10; i++) 
-    var num = math.floor(math.random() * 10) + 1;
+  var charNumber = prompt("Choose number of characters for password (between 8-128)");
+  if (charNumber < 8) 
+  {
+    alert ("Password is too short");
+    return;
+  } 
+  if (charNumber > 128) {
+    alert ("Password is too long");
+    return;
   }
 
+  console.log (charNumber);
 
-  passwordText.value = password;
+  var confirmNumbers = confirm("Click ok to include numbers");
+  if(confirmNumbers) {
+    userChar.push(...numbers);
+  }
+  console.log(userChar);
 
+  var confirmLower = confirm("Click ok to include lower case letters");
+  if(confirmLower) {
+    userChar.push(...lowercase);
+
+    console.log(userChar);
+  }
+  var confirmUpper = confirm ("Click ok to include uppercase letters");
+  if(confirmUpper) {
+    userChar.push(...uppercase);
+
+    console.log(userChar);
+  }
+  var confirmSpecial = confirm ("Click ok to include special characters");
+  if(confirmSpecial) {
+    userChar.push(...special);
+
+    console.log(userChar);
+  }
+
+  for ( let i = 0; i < charNumber; i++ ) {
+    userPass.push(userChar[Math.floor(Math.random() * userChar.length)]);
+    console.log(userPass.join('+'));
+  }
+
+  document.getElementById("password").value = userPass.join("");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
